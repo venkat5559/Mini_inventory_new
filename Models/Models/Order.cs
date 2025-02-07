2ee5872a876133b2fd8994ab968d82b9f300500a
venkat5559 <9840730616benmax@gmail.com>
2025-02-07 15:55:22 +0530

    Add files via upload

---
+++ Models/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mini_Inventory_Management_System.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public string ProductName {get;set;}
+
+        public DateTime OrderDate { get;set; }
+    }
+}
